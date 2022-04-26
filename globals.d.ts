c009470dcf2fb8ceb85fc6a4ea09508f98e1fe2d
Björn Brauer <bjoern.brauer@new-work.se>
2022-04-26 16:07:56 +0000

    feat: use tinygo instead of go to create smaller wasm binary
    This commit refactors the wasm and JavaScript parts to make use of
    TinyGo for creating even smaller wasm binaries.
    It also introduces another wasm-opt pass that shaves off ~500K.
    
    The API has changed: Previously the exported `createLinter()` function
    was synchronous and returned an async function to lint a single file.
    Now the `createLinter()` function is asynchronous and the returned
    linter function is synchronous.
    
    ```js
    import { createLinter } from "actionlint";
    const linter = await createLinter();
    const results = linter("on: psuh", "borked.yml");
    ```
    
    BREAKING CHANGE: The `createLinter()` function is now asynchronous and
    resolves to a function that synchronously invokes the actionlint binary.

--- globals.d.ts
+++
@@ -1,6 +0,0 @@
-export declare global {
-  var runActionlint:
-    | ((src: string, path: string) => [LintResult[], Error | null])
-    | undefined;
-  var actionlintInitialized: (() => void) | undefined;
-}
--- test.mjs
+++ test.mjs
@@ -5,10 +5,10 @@ import { createLinter } from "./node.cjs";
 test("actionlint no errors", async (t) => {
   t.plan(1);
 
-  const linter = createLinter();
+  const linter = await createLinter();
 
   const content = await readFile("./.github/workflows/push.yml", "utf8");
-  const result = await linter(content, "./.github/workflows/push.yml");
+  const result = linter(content, "./.github/workflows/push.yml");
 
   t.equal(result.length, 0, "no errors");
 
@@ -16,13 +16,30 @@ test("actionlint no errors", async (t) => {
 });
 
 test("actionlint errors", async (t) => {
-  t.plan(1);
+  t.plan(3);
 
-  const linter = createLinter();
+  const linter = await createLinter();
 
-  const result = await linter("on: psuh", "broken.yml");
+  const result = linter("on: psuh", "broken.yml");
 
   t.equal(result.length, 2, "2 errors");
+
+  t.deepEqual(result[0], {
+    file: "broken.yml",
+    line: 1,
+    column: 1,
+    message: '"jobs" section is missing in workflow',
+    kind: "syntax-check",
+  });
+
+  t.deepEqual(result[1], {
+    file: "broken.yml",
+    line: 1,
+    column: 5,
+    message:
+      'unknown Webhook event "psuh". see https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows#webhook-events for list of all Webhook event names',
+    kind: "events",
+  });
 
   t.end();
 });
